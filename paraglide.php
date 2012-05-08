96ff463d9350177b1fed4d1f9c84e7e4e9b646d6
brandon <brandon@misomedia.com>
2012-05-07 18:35:03 -0700

    add minimalist controller class

--- paraglide.php
+++ paraglide.php
@@ -449,7 +449,12 @@ class Paraglide {
 		// perform routing
 		$GLOBALS['arguments'] = array();
 		$arguments = explode('/', $location);
-		$nested_dirs = $arguments;
+		$nested_dirs = array();
+		
+		foreach ($arguments as $argument) {
+			if (strlen($argument) > 0) $nested_dirs[] = $argument;
+		}
+		
 		$controller = DEFAULT_CONTROLLER;
 		self::$nested_dir = '';
 		
@@ -794,11 +799,36 @@ class Paraglide {
 }
 
 class Controller {
-	public function render_view($view, $data, $buffer = false) {
+	private $_slug;
+	
+	public function __construct() {
 		$controller = get_class($this);
 		$controller_underscore = Paraglide::inflect_underscore($controller);
-		$prefix = substr($controller_underscore, 0, -strlen('_controller'));
-		$view = $prefix . '/' . $view;
-		return Paraglide::render_view($view, $data, $buffer);
+		
+		if (substr($controller_underscore, -strlen('_controller')) == '_controller') {
+			$controller_underscore = substr($controller_underscore, 0, -strlen('_controller'));
+		}
+		
+		$this->_slug = Paraglide::$nested_dir;
+
+		if ($controller_underscore != DEFAULT_CONTROLLER) {
+			// we don't want to append the main controller's name to the url
+			$this->_slug .= $controller_underscore;
+		} else {
+			// remove the trailing /
+			$this->_slug = substr($this->_slug, 0, -1);
+		}
+	}
+	
+	public function redirect($action = null, $params = null, $query_string = null, $ssl = false) {
+		return Paraglide::redirect($this->_slug, $action, $params, $query_string, $ssl);
+	}
+	
+	public function render_view($view, $data, $buffer = false) {
+		return Paraglide::render_view($this->_slug . '/' . $view, $data, $buffer);
+	}
+	
+	public function url($action = null, $params = null, $query_string = null, $ssl = false) {
+		return Paraglide::url($this->_slug, $action, $params, $query_string, $ssl);
 	}
 }
